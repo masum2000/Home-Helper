65d812323568d0df97d35a6410da30ddabb3e20e
Masum <masumt999.com>
2023-04-09 16:20:32 +0600

    responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
       <div className='relative flex items-center justify-between'>
         <Link
           to='/'
-          aria-label='HeroGadget'
+          aria-label='HomeHelper'
           title='HomeHelper'
           className='inline-flex items-center'
         >
@@ -102,22 +102,23 @@ const Header = () => {
                   <div>
                     <Link
                       to='/'
-                      aria-label='HeroGadget'
-                      title='HeroGadget'
+                      aria-label='HomeHelper'
+                      title='HomeHelper'
                       className='inline-flex items-center'
                     >
                       <div className='flex items-center justify-center w-8 h-8 rounded-full bg-indigo-200'>
-                        <svg
-                          xmlns='http://www.w3.org/2000/svg'
-                          viewBox='0 0 32 32'
-                          fill='currentColor'
-                          className='flex-shrink-0 w-5 h-5 rounded-full text-indigo-600'
-                        >
-                          <path d='M18.266 26.068l7.839-7.854 4.469 4.479c1.859 1.859 1.859 4.875 0 6.734l-1.104 1.104c-1.859 1.865-4.875 1.865-6.734 0zM30.563 2.531l-1.109-1.104c-1.859-1.859-4.875-1.859-6.734 0l-6.719 6.734-6.734-6.734c-1.859-1.859-4.875-1.859-6.734 0l-1.104 1.104c-1.859 1.859-1.859 4.875 0 6.734l6.734 6.734-6.734 6.734c-1.859 1.859-1.859 4.875 0 6.734l1.104 1.104c1.859 1.859 4.875 1.859 6.734 0l21.307-21.307c1.859-1.859 1.859-4.875 0-6.734z'></path>
-                        </svg>
+                      <svg 
+                         xmlns="http://www.w3.org/2000/svg" 
+                         fill="none" viewBox="0 0 24 24" 
+                         stroke-width="1.5" stroke="currentColor" 
+                         class="w-6 h-6"
+                       >
+                         <path stroke-linecap="round" 
+                          stroke-linejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819" />
+                   </svg>
                       </div>
-                      <span className='ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase'>
-                        HG
+                      <span className='ml-2 text-xl font-bold tracking-wide text-gray-800'>
+                        HomeHelper
                       </span>
                     </Link>
                   </div>
@@ -151,6 +152,16 @@ const Header = () => {
                     </li>
                     <li>
                       <Link
+                        to='/about'
+                        aria-label='About Us'
+                        title='About Us'
+                        className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'
+                      >
+                        About Us
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
                         to='/cart'
                         aria-label='Cart'
                         title='Cart'
@@ -162,16 +173,6 @@ const Header = () => {
                             {cart.length}
                           </p>
                         </div>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        to='/about'
-                        aria-label='About Us'
-                        title='About Us'
-                        className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'
-                      >
-                        About Us
                       </Link>
                     </li>
                   </ul>
